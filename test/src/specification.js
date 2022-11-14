796b7fefd4f26981673781c3ac94148e00e60c62
a flying potato <80830782+a-flying-potato@users.noreply.github.com>
2022-11-14 20:42:09 +0100

    :robot: refactor: Use ESM import syntax.
    These changes were automatically generated by a transform whose code can be found at:
      - https://github.com/make-github-pseudonymous-again/rejuvenate/blob/9b50c2a1d7e9ab7b4bc9c9ff88892e98c5f62244/src/transforms/codemod:use-esm-import-syntax.js
    Please contact the author of the transform if you believe there was an error.

--- test/src/specification.js
+++ test/src/specification.js
@@ -2,7 +2,7 @@ import test from 'ava';
 
 import * as spec from "@graph-data-structure/specification" ;
 
-import {Graph, DiGraph} from '../../src';
+import {Graph, DiGraph} from '../../src/index.js';
 
 spec.graph( test, "Adjacency Matrix Graph" , Graph ) ;
 spec.digraph( test, "Adjacency Matrix DiGraph" , DiGraph ) ;
