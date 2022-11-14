438b023c09e99a82cc170895e6ddbaed5ba96c4b
a flying potato <80830782+a-flying-potato@users.noreply.github.com>
2022-11-14 20:20:04 +0100

    :robot: chore: Patch tests.

--- test/src/spec.js
+++ test/src/spec.js
@@ -1,5 +1,7 @@
+import test from 'ava';
+import * as adjacencymatrix from '../../src';
 
-var spec = require( "@aureooms/js-graph-spec" ) ;
+import spec from "@aureooms/js-graph-spec" ;
 
 spec.Graph( "Adjacency Matrix Graph" , adjacencymatrix.Graph ) ;
 spec.DiGraph( "Adjacency Matrix DiGraph" , adjacencymatrix.DiGraph ) ;
